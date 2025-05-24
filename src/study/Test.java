8e165065244f6e607c7f71d1c5e4477d9df2268a
zhangbriangit <568496890@qq.com>
2025-05-24 21:47:00 +0800

    upload

---
+++ src/study/Test.java
@@ -0,0 +1,26 @@
+package study;
+
+class test {
+
+}
+
+//12.Output of the below Java Code?
+class Base {
+	public final void show() {
+		System.out.println("Base::show() called");
+	}
+}
+
+class Derived extends Base {
+	public void show1() {
+		System.out.println("Derived::show() called");
+	}
+}
+
+//public 
+class Main1 {
+	public static void main(String[] args) {
+		Base b = new Derived();		;
+		b.show();
+	}
+}
